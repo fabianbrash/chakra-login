c0a4593683cfcabf47c24b58a1ae272ce390aac6
Fabian Brash <fabianbrash@gmail.com>
2020-09-08 18:39:22 -0400

    added multiple users

--- src/utils/mockApi.js
+++ src/utils/mockApi.js
@@ -3,7 +3,7 @@ export const userLogin = async ({email, password}) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
 
-            if (email === 'test@test.com' && password === 'password') {
+            if ((email === 'test@test.com' && password === 'password') || (email === 'admin@local' && password === 'password')) {
                 resolve();
             } else {
                 reject();
